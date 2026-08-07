9f6fb0fa246403fbd6d4465e345903d5df0b55d7
Mototown <105690902+Mototown@users.noreply.github.com>
2026-08-06 22:14:34 -0700

    Delete agent.py

--- agent.py
+++
@@ -1,6 +0,0 @@
-from.generator import BloomGenerator
-class BloomAgent:
-    def __init__(self):
-        self.generator = BloomGenerator()
-    def run(self, business_question: str, record):
-        return self.generator.generate_model(business_question, record)
